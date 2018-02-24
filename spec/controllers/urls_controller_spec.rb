282e274c533588bc312d3f1241c520b5ab0e3822
Ife Runsewe <ife.runsewe@simplybusiness.co.uk>
2018-02-24 09:17:08 +0000

    placeholder for controller method short_url

--- spec/controllers/urls_controller_spec.rb
+++ spec/controllers/urls_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe UrlsController, type: :controller do
+  describe '#short_url' do
+    context 'POST request to /short_url' do
+      it 'returns a shortened url' do
+
+      end
+
+      it 'returns the original url' do
+
+      end
+
+      it 'returns status code 200' do
+        
+      end
+    end
+  end
 
 end
