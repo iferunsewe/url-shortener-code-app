b6701c241108bc81c8c889af5b4edde8f0423457
Ife Runsewe <ife.runsewe@simplybusiness.co.uk>
2018-02-24 09:39:46 +0000

    Added before request

--- spec/controllers/urls_controller_spec.rb
+++ spec/controllers/urls_controller_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe UrlsController, type: :controller do
   describe '#short_url' do
+    before do
+      headers = { "CONTENT_TYPE" => "application/json" }
+      request.headers.merge! headers
+      post 'short_url', params: {url: 'http://www.looongurl.com'}
+    end
+
     context 'POST request to /short_url' do
       it 'returns a shortened url' do
 
