7aaed010cb44f123a777c062d0e7c9bada661f66
Ife Runsewe <ife.runsewe@simplybusiness.co.uk>
2018-03-05 11:00:34 +0000

    Added route for redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'short_url', to: 'urls#short_url'
+
+  get '/:short_url', to: 'urls#redirect_to_original'
 end
