6737ffa067ff0be3dd71d21e69b1b8773f00ff1a
Ife Runsewe <ife.runsewe@simplybusiness.co.uk>
2018-03-05 13:35:10 +0000

    added model tests

--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Url, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:original) }
+
+  it 'can generate a random alpha numeric string' do
+    alpha_string = described_class.generate_random_alpha_numeric_string(6)
+    expect(alpha_string).to match(/^[a-zA-Z0-9]{6,}$/)
+  end
 end
