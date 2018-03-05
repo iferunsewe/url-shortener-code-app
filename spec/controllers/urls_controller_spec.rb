1629deb132e3c37b7da65893a59c6c5e9dfdbfb4
Ife Runsewe <ife.runsewe@simplybusiness.co.uk>
2018-03-05 10:14:48 +0000

    controller tests passing

--- spec/controllers/urls_controller_spec.rb
+++ spec/controllers/urls_controller_spec.rb
@@ -1,20 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe UrlsController, type: :controller do
+  render_views
+
   describe '#short_url' do
     before do
       headers = { "Content-Type" => "application/json" }
       request.headers.merge! headers
-      post 'short_url', {params: {url: 'http://www.looongurl.com'}, format: :json}
+      post 'short_url', {params: {url: original_url}, format: :json}
     end
+
+    let(:parsed_json){  JSON.parse(response.body) }
+    let(:original_url) { 'http://www.looongurl.com' }
 
     context 'POST request to /short_url' do
       it 'returns a shortened url' do
-        expect(response[:url]).to exist
+        expect(parsed_json['short_url']).to match(/https?:\/\/[\S]+/)
       end
 
       it 'returns the original url' do
-        expect(response[:short_url]).to exist
+        expect(parsed_json['url']).to eq original_url
+      end
+
+      it 'renders a template' do
+        expect(response).to render_template(:short_url)
       end
 
       it 'returns status code 200' do
@@ -22,5 +31,4 @@ RSpec.describe UrlsController, type: :controller do
       end
     end
   end
-
 end
